232c6f5ecf5c937a15e691eba1f8b355672f044a
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-05-01 15:34:02 +0800

    props

--- src/index.js
+++ src/index.js
@@ -2,33 +2,58 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-/////////////COMPONENTS///////////////////////
+///VARIABLES////
 const title = `JavaScript: The Definitive Guide: Master the World's Most-Used
   Programming Language`;
-const img = `./img/JS2.jpg" alt="Javascript`;
+const img = `./img/JS2.jpg`;
 const author = `David Flanagan`;
 
+/////////////COMPONENTS///////////////////////
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book job="developer" />
+      <Book title="king" number={22} />
     </section>
   );
 };
 
-const Book = () => {
+// function BookList() {
+//   return (
+//     <section className='booklist'>
+//       <Book job='developer' />
+//       <Book title='random title' number={22} />
+//     </section>
+//   );
+// }
+
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img src="./img/JS2.jpg" alt="Javascript"></img>
+      <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author.toUpperCase()}</h4>
+      <p>{props.title}</p>
+      <p>{props.job}</p>
+      {console.log(props)}
     </article>
   );
 };
 
+// const Book = (props) => {
+//   console.log(props);
+//   return (
+//     <article className="book">
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h4>{author} </h4>
+//       <p>{props.job}</p>
+//       <p>{props.title}</p>
+//       <p>{props.number}</p>
+//     </article>
+//   );
+// };
 /////////////PRINT///////////////////////
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
